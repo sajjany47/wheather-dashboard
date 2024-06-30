dcfe1ce944c758780afd33760ba7153cc8638a83
sajjany47 <sajjany47@gmail.com>
2024-06-30 21:16:44 +0530

    changes

--- src/WheatherReport.jsx
+++ src/WheatherReport.jsx
@@ -8,18 +8,38 @@ import { GiWindSlap } from "react-icons/gi";
 import { VscCompass } from "react-icons/vsc";
 import { FaDharmachakra } from "react-icons/fa6";
 
-const WheatherReport = (index) => {
-  let aciveIndex = index.index;
+const WheatherReport = () => {
+  // let aciveIndex = index.index;
 
   const apiService = new ApiService();
   const [value, setValue] = useState("");
   const [items, setItems] = useState([]);
   const [data, setData] = useState({});
+  const [futureData, setFutureData] = useState({});
 
   useEffect(() => {
-    fetchData(aciveIndex);
+    // fetchData(aciveIndex);
+    dataFetch();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const dataFetch = () => {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+
+    apiService.forcastDetails("kolkata", 3).then((res) => {
+      const findTodayData = res.forecast.forecastday.find(
+        (item) => item.date === moment(new Date()).format("YYYY-MM-DD")
+      );
+      const filterData = res.forecast.forecastday.filter(
+        (item) => item.date !== moment(new Date()).format("YYYY-MM-DD")
+      );
+      console.log(filterData);
+      setData({ ...res, forecast: findTodayData });
+      setFutureData({ ...res, forecast: filterData });
+    });
+  };
+
   const search = (event) => {
     apiService
       .searchReason(event.query)
@@ -29,33 +49,32 @@ const WheatherReport = (index) => {
       .catch((error) => console.log(error));
   };
 
-  const fetchData = (aciveIndex) => {
-    const date = new Date();
-    date.setDate(date.getDate() - 1);
+  // const fetchData = (aciveIndex) => {
+  //   const date = new Date();
+  //   date.setDate(date.getDate() - 1);
 
-    if (aciveIndex === 0) {
-      apiService
-        .historyWheather("kolkata", moment(date).format("YYYY-MM-DD"))
-        .then((res) => {
-          setData({ ...res, forecast: res.forecast.forecastday[0] });
-        })
-        .catch((error) => console.log(error));
-    }
-    if (aciveIndex === 1 || aciveIndex === 2) {
-      apiService
-        .forcastDetails("kolkata", aciveIndex === 1 ? 1 : 2)
-        .then((res) => {
-          if (aciveIndex === 1) {
-            setData({ ...res, forecast: res.forecast.forecastday[0] });
-          } else {
-            setData({ ...res, forecast: res.forecast.forecastday[1] });
-          }
-        })
-        .catch((error) => console.log(error));
-    }
-  };
+  //   if (aciveIndex === 0) {
+  //     apiService
+  //       .historyWheather("kolkata", moment(date).format("YYYY-MM-DD"))
+  //       .then((res) => {
+  //         setData({ ...res, forecast: res.forecast.forecastday[0] });
+  //       })
+  //       .catch((error) => console.log(error));
+  //   }
+  //   if (aciveIndex === 1 || aciveIndex === 2) {
+  //     apiService
+  //       .forcastDetails("kolkata", aciveIndex === 1 ? 1 : 2)
+  //       .then((res) => {
+  //         if (aciveIndex === 1) {
+  //           setData({ ...res, forecast: res.forecast.forecastday[0] });
+  //         } else {
+  //           setData({ ...res, forecast: res.forecast.forecastday[1] });
+  //         }
+  //       })
+  //       .catch((error) => console.log(error));
+  //   }
+  // };
 
-  console.log(data);
   return (
     <>
       <div className="flex justify-content-end">
@@ -70,7 +89,7 @@ const WheatherReport = (index) => {
       </div>
       {Object.keys(data).length > 0 && (
         <div className="grid">
-          <div className="sm:col-12 md:col-4">
+          <div className="sm:col-12 md:col-4 mt-2">
             <h4>
               {data.location.name} ,{data.location.region} ,
               {data.location.country}
@@ -80,8 +99,8 @@ const WheatherReport = (index) => {
             <p>{moment(data.current.last_updated).format("Do MMM , YYYY")}</p>
           </div>
           <div className="sm:col-12 md:col-8">
-            <div className="flex">
-              <div>
+            <div className="grid mt-3">
+              <div className="sm:col-12 md:col-4">
                 <h1>{data.current.temp_c} °C</h1>
                 <span>Feels like</span> : {data.current.feelslike_c}
                 <div className="flex flex-column">
@@ -101,17 +120,25 @@ const WheatherReport = (index) => {
                   </div>
                 </div>
               </div>
-              <div className="ml-8">
-                <img
-                  src={data.current.condition.icon}
-                  alt=""
-                  style={{ height: "150px" }}
-                />
-                <h3 style={{ width: "150px" }}>
-                  {data.current.condition.text}
-                </h3>
+              <div className="sm:col-12 md:col-4 ">
+                <div className="grid">
+                  <div className="col-12 mr-4 ">
+                    {" "}
+                    <img
+                      src={data.current.condition.icon}
+                      alt=""
+                      style={{ height: "150px" }}
+                    />
+                  </div>
+                  <div
+                    className="col-12 font-semibold"
+                    style={{ width: "60px" }}
+                  >
+                    {data.current.condition.text}
+                  </div>
+                </div>
               </div>
-              <div className="ml-5">
+              <div className="sm:col-12 md:col-4 ">
                 <div className="flex gap-4">
                   <div>
                     <WiHumidity size={60} />
